95dc829550706db96b893dd696c6523122225cbb
lianginh <lianginh@qq.com>
2019-11-30 05:30:04 +0800

    12

---
+++ a.js
@@ -0,0 +1,9 @@
+portfolio1.onclick =function(){
+    bart.className ="bar start"
+};
+portfolio2.onclick =function(){
+    bart.className ="bar start1"
+};
+portfolio3.onclick =function(){
+    bart.className ="bar start2"
+};
